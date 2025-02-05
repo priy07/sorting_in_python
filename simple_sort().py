26687747b308f75f7ce72d7289f3bec05adcaa67
Priyanshi Dwivedi <138594939+priy07@users.noreply.github.com>
2025-02-05 18:30:08 +0530

    Update simple_sort().py

--- simple_sort().py
+++ simple_sort().py
@@ -1,5 +1,11 @@
-list = [5, 3, 1, 2, 4]
-print(list)
- 
-list.sort()
-print(list)
+#creating a random no of list
+my_list = [5, 3, 1, 2, 4]
+print(my_list)
+
+#sorting the list using sort()
+my_list.sort()
+print(my_list)
+
+#reversing the sorted list
+my_list.reverse()
+print(my_list)
